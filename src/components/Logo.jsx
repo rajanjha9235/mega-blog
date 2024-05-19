8c12345411240e7b1e223661239225d053d5517a
rajanjha9235 <rajanjha9235@gmail.com>
2024-05-19 11:30:20 +0530

    Changed Logo PAth

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,5 +1,5 @@
 // Desc: Logo component
-import logo from "/home/rajanjha/Pictures/logo.png";
+import logo from "../assets/logo.png"
 function Logo({width = '100px'}){
     return(
         <img src={logo} alt="Logo Image" width={width} className="rounded-full animate-mymove"/>
